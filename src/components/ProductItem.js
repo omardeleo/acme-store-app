47efd897b6361e5c10c89071faf7e3a85d3755b2
Omar De Leo <omar.deleo@gmail.com>
2019-03-22 12:36:07 -0400

    Update markup

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -5,16 +5,18 @@ import Product from './Product'
 const ProductItem = ({ product, onAddToCartClicked }) => (
   <div className="product-item-container" style={{ marginBottom: 20 }}>
     <img className="product-image" alt={product.title} src={"/assets/images/" + product.title.toLowerCase() + ".svg"} />
-    <Product
-      title={product.title}
-      price={product.price}
-      inventory={product.inventory} />
-    <button
-      className="product-button"
-      onClick={onAddToCartClicked}
-      disabled={product.inventory > 0 ? '' : 'disabled'}>
-      {product.inventory > 0 ? 'ADD TO CART' : 'SOLD OUT'}
-    </button>
+    <div className="product-column-1">
+      <Product
+        title={product.title}
+        price={product.price}
+        inventory={product.inventory} />
+      <button
+        className="product-button"
+        onClick={onAddToCartClicked}
+        disabled={product.inventory > 0 ? '' : 'disabled'}>
+        {product.inventory > 0 ? 'ADD TO CART' : 'SOLD OUT'}
+      </button>
+    </div>
   </div>
 )
 
