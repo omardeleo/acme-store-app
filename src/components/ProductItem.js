d68dc41b9d86808cca11d344bb378681c09a20b2
Omar De Leo <omar.deleo@gmail.com>
2019-03-25 11:55:35 -0400

    Set up style for second breakpoint

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,23 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Product from './Product'
 
-const ProductItem = ({ product, onAddToCartClicked }) => (
-  <div className="product-item-container" style={{ marginBottom: 20 }}>
-    <img className="product-image" alt={product.title} src={"/assets/images/" + product.title.toLowerCase() + ".svg"} />
-    <div className="product-column-1">
-      <Product
-        title={product.title}
-        price={product.price}
-        inventory={product.inventory} />
-      <button
-        className="product-button"
-        onClick={onAddToCartClicked}
-        disabled={product.inventory > 0 ? '' : 'disabled'}>
-        {product.inventory > 0 ? 'ADD TO CART' : 'SOLD OUT'}
-      </button>
+const ProductItem = ({ product, onAddToCartClicked }) => {
+  const imageName = `/assets/images/${product.title.toLowerCase()}.jpg`;
+  return (
+    <div className="product-item-container" style={{ marginBottom: 20 }}>
+      <img className="product-image" 
+        src={`${imageName}`} 
+      />
+      <div className="product-column-1">
+        <Product
+          title={product.title}
+          price={product.price}
+          inventory={product.inventory} />
+        <button
+          className="product-button"
+          onClick={onAddToCartClicked}
+          disabled={product.inventory > 0 ? '' : 'disabled'}>
+          {product.inventory > 0 ? 'ADD TO CART' : 'SOLD OUT'}
+        </button>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 ProductItem.propTypes = {
   product: PropTypes.shape({
