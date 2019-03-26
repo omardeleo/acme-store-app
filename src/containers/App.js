f0dfabe6dbf3bfaf75692a3ddb2d244b0d244a2b
Omar De Leo <omar.deleo@gmail.com>
2019-03-25 22:32:37 -0400

    Map cart state to props

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
 
-const App = ({store}) => (
-  <div className="main">
-    <h2 className="header">Acme Store</h2>
-    <div className="hr" />
-    <ProductsContainer />
-    <hr />
-    <CartContainer store={store} />
-  </div>
-)
+const App = ({store, isCartOpen}) => {
+  return (
+    <div className="main">
+      <div className="header-section">
+        <h2 className="header">Acme Store</h2>
+        <button onClick={() => store.dispatch({type: "TOGGLE_CART"})}>Shopping Cart</button>
+      </div>
+      <div className="hr" />
+      <ProductsContainer />
+      <hr />
+      {isCartOpen ? <CartContainer /> : ''}
+    </div>
+  )
+}
 
-export default App
+const mapStateToProps = (state) => ({
+  isCartOpen: state.cart.isCartOpen
+})
+
+export default connect(
+  mapStateToProps,
+  null
+)(App)
+
